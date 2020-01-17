8b16651e7cc2c019ceb2f278c674cdaef2bfe64c
sothearith <thearith6800@gmail.com>
2020-01-17 19:47:14 +0700

    gh-pages

---
+++ precache-manifest.838676ebcbc67ab32b3914a146ea4b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13587014bc68f19ca7ce20cee878c132",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "37c36567f69bd0fff158",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "036957d44fa1eac81f31",
+    "url": "/react-app/static/js/2.f4cd3d9f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.f4cd3d9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37c36567f69bd0fff158",
+    "url": "/react-app/static/js/main.a81e4899.chunk.js"
+  },
+  {
+    "revision": "74e9287c2e88d478e0df",
+    "url": "/react-app/static/js/runtime-main.f1193148.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
